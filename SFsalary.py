ba1cd9781b5d946e12c7547bb004dcb996ed4131
satosys <satya9pinu@gmail.com>
2018-04-29 16:05:28 -0400

    ok.

--- SFsalary.py
+++ SFsalary.py
@@ -1,4 +1,12 @@
 #Assignment on SF Salary datset from Kaggle.
 
 import pandas as pd
-pd.read_csv('/Users/SatsRocks9/Desktop/Salaries.csv')
+sal = pd.read_csv('/Users/SatsRocks9/Desktop/Salaries.csv')
+#print (open('/Users/SatsRocks9/Desktop/Salaries.csv').read())
+
+print(sal.head())
+#print(sal.info())
+sal_mean_sum = sal['BasePay'].mean()
+print(sal_mean_sum)
+sal_max_pay = sal['OvertimePay'].max()
+print(sal_max_pay)
