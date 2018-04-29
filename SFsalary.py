7939a77d8592c745098de8dda9623102d9394aa4
satosys <satya9pinu@gmail.com>
2018-04-29 15:30:46 -0400

    ok.

--- SFsalary.py
+++ SFsalary.py
@@ -1,4 +1,4 @@
 #Assignment on SF Salary datset from Kaggle.
 
 import pandas as pd
-pd.read_csv('/Users/SatsRocks9/Downloads/Python-Data-Science-and-Machine-Learning-Bootcamp/Python-for-Data-Analysis/Pandas/Pandas Exercises/Salaries.csv')
+pd.read_csv('/Users/SatsRocks9/Desktop/Salaries.csv')
